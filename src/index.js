ef810140a71b0007c0fa87d9a518391e24c11ce5
LordMorvak <lordmorvaky@gmail.com>
2023-12-27 22:39:26 -0300

    Terceiro commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log("oi")
+let nomeHeroi = "LordMorvak"
+let quantidadeXp = 7001
+let nivelXp = ""
+
+if (quantidadeXp <= 1000) {
+    nivelXp = "Ferro"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+    nivelXp = "Bronze"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+    nivelXp = "Prata"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
+    nivelXp = "Ouro"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
+    nivelXp = "Platina"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
+    nivelXp = "Ascendente"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+    nivelXp = "Imortal"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+else if (quantidadeXp >= 10001) {
+    nivelXp = "Radiante"
+    console.log("O Heroi de nome: " + nomeHeroi + " esta no nivel de: " + nivelXp)
+
+}
+
+
+
